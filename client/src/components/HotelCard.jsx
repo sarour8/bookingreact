46736affc8e72bc222541cc6945b1b2c6e2acc4f
sarour8 <chrifaelmaher43@gmail.com>
2025-11-10 13:28:57 +0100

    Update HotelCard.jsx

--- client/src/components/HotelCard.jsx
+++ client/src/components/HotelCard.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 import { assets } from '../assets/assets';
 const HotelCard = ({room, index}) => {
     return (
-        <Link to={'/rooms/' + room._id} onClick={() => scrollTo(0,0) } key={room._id}>
-<img src={room.images[0]} alt=""    className='relative max-w-70 w-full 
-rounded-xl overflow-hidden bg-white text-gray-500/90 shadow-[0px_4px_4px_rgba(0,0,0,0.05)]'/>
+        <Link to={'/rooms/' + room._id} onClick={() => scrollTo(0,0) } key={room._id} className='relative max-w-70 w-full 
+rounded-xl overflow-hidden bg-white text-gray-500/90 shadow-[0px_4px_4px_rgba(0,0,0,0.05)]' >
+<img src={room.images[0]} alt=""    />
   
   
   { index % 2 === 0 && <p className='px-3 py-1 absolute top-3 left-3 text-xs bg-white 
