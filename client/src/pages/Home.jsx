f77b7e041d2b4fb446a909b3830fd7f46ffde159
sarour8 <chrifaelmaher43@gmail.com>
2025-11-10 14:20:03 +0100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero";
 import FeatureDestination from "../components/FeatureDestination";
+import ExclusiveOffers from "../components/ExclusiveOffers";
 const Home = () => {
     return(
         <>
            <Hero />
            <FeatureDestination />
+           <ExclusiveOffers />
         </>
     )
 }
